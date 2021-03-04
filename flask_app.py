7149de57f85f1841859e87c45ade8fa83bef9f9b
samuelwang23 <swang2023@gmail.com>
2021-03-04 15:17:16 -0500

    Review Posting Endpoint
    Added an endpoint for posting reviews of the summaries for a model. The endpoint takes in a review and adds it to a CSV.

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,8 @@ from flask_cors import CORS, cross_origin
 import os, random
 from cluster_summarizer import summarize
 from consts import __CLUSTER_TYPES__
+import pandas as pd
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -18,7 +20,7 @@ def _get_file_contents(file_name):
 			break
 		else:
 			file_contents += line
-	return file_contents
+	return file_contents.replace(" . . .", "")
 
 @app.route('/')
 def hello_world():
@@ -30,6 +32,9 @@ def get_random_test():
     results = {}
     results["path"] = get_random_text()
     results["text"] = _get_file_contents(results["path"])
+    while len(results["text"]) > 2700 or len(results["text"]) < 1300:
+        results["path"] = get_random_text()
+        results["text"] = _get_file_contents(results["path"])
     results["cluster_type_1"] = random.choice(__CLUSTER_TYPES__)
     results["summary_1"] = summarize(results["text"], results["cluster_type_1"])
     results["cluster_type_2"] = random.choice(__CLUSTER_TYPES__)
@@ -43,3 +48,18 @@ def get_random_text():
     parent_path = "/home/swang/TextSummarizer/static/Sections"
     random_chapter_path = os.path.join(parent_path, random.choice(os.listdir(parent_path)))
     return os.path.join(random_chapter_path, random.choice(os.listdir(random_chapter_path)))
+
+@app.route('/api/v1/reviews', methods=['POST'])
+def post_reviews():
+    review_json = request.args.get('review')
+    if review_json:
+        reviews = pd.read_csv('/home/swang/TextSummarizer/reviews.csv')
+
+        review = pd.read_json(review_json, typ='series', orient='records')
+
+        reviews = reviews.append(review, ignore_index=True)
+
+        reviews.to_csv("/home/swang/TextSummarizer/reviews.csv", header = True, index = False)
+        return "The review was added."
+
+    return "Your request was not valid. Please try again."
